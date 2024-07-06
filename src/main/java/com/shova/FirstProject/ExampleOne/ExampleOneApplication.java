628b592b3262dd21bd5396cfbd35d2c4e287cdc6
Shova Yakami <shovayakamionline@gmail.com>
2024-07-06 17:50:14 -0400

    GLAB - 309.2.1 - SP1 - How to use the Spring Initializr to set up a Spring Boot Project

---
+++ src/main/java/com/shova/FirstProject/ExampleOne/ExampleOneApplication.java
@@ -0,0 +1,14 @@
+package com.shova.FirstProject.ExampleOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleOneApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExampleOneApplication.class, args);
+	}
+
+}
